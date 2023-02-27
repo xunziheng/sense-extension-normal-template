21646a31af1b948ef4d8b9b5dd3b5016faf0178c
xunziheng <xunziheng@gmail.com>
2023-02-27 14:36:10 +0800

    initial

---
+++ README.md
@@ -0,0 +1,3 @@
+1. Globally search for "{{extension-name}}", then replace with yours;
+2. npm install;
+3. npm run watch-qext-scripts;
---
+++ src/methods/definition.js
@@ -0,0 +1,21 @@
+export default {
+  type: "items",
+  component: "accordion",
+  items: {
+    dimensions: {
+      uses: "dimensions",
+      min: 1,
+      max: 1
+    },
+    measures: {
+      uses: "measures",
+      min: 1,
+      max: 1
+    },
+    settings: {
+      uses: "settings",
+      items: {
+      }
+    }
+  }
+};
